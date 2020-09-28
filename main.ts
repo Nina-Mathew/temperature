f18b6cd51ee2c2f0f6561169b857544ddb57ecf3
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-09-28 13:07:38 -0400

    temperature

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+let current_temperature = 0
 // Created by: Nina Mathew
 // Created on: 9/28/20
 // This program uses Variables
 basic.forever(function () {
-	
+    current_temperature = input.temperature()
+    basic.showNumber(current_temperature)
+    basic.pause(1000)
+    basic.clearScreen()
 })
